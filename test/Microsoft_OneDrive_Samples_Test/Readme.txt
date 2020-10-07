984efe95b88c9fb453be1ae0f16902fbff6c39ed
fero <feroalien@hotmail.com>
2020-10-07 15:45:05 +1000

    OneDrive sample

---
+++ test/Microsoft_OneDrive_Samples_Test/Readme.txt
@@ -0,0 +1,7 @@
+﻿
+
+## Resources
+https://docs.microsoft.com/en-us/onedrive/developer/?view=odsp-graph-online
+https://github.com/OneDrive
+https://github.com/OneDrive/onedrive-community-samples
+https://github.com/microsoftgraph/msgraph-sdk-dotnet-auth
---
+++ test/Microsoft_OneDrive_Samples_Test/SampleTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Graph;
+using Microsoft.Graph.Auth;
+using Microsoft.Identity.Client;
+using Microsoft_OneDrive_Samples_Test.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Microsoft_OneDrive_Samples_Test
+{
+    public class SampleTest
+    {
+        public const string ClientId = "";  // Put clientId there
+        public const string TenantId = "";  // Put tenantId there
+        public const string Secret = "";  // Put secret there
+        public static string[] Scopes = new[] { "User.Read", "Files.ReadWrite" };
+
+        [Fact]
+        public async void GetAccountInfo()
+        {
+            IConfidentialClientApplication confidentialClientApplication = ConfidentialClientApplicationBuilder
+                 .Create(ClientId)
+                 .WithTenantId(TenantId)
+                 .WithClientSecret(Secret)
+                 .Build();
+
+            var authenticationProvider = new ClientCredentialProvider(confidentialClientApplication);
+            var graphClient = new GraphServiceClient(authenticationProvider);
+
+            var me = await graphClient.Me.Request().WithForceRefresh(true).GetAsync();
+
+            Assert.NotNull(me);
+        }
+    }
+}
